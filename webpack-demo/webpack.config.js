1209db2e279bb80bb2f838038ba07d93b1c2f468
MuzLin <1030996014@qq.com>
2018-06-12 14:15:52 +0800

    e

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -25,6 +25,15 @@ module.exports = {
       use:[
         'file-loader'
       ]
+    },{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
     }]
   }
 }
